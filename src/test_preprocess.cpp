ecb58ac3d42dccf66bddd8bac4060fa3e65060b9
ZhangYou0122 <andyfreshair@gmail.com>
2014-02-22 01:08:00 +0800

    preprocess logs

---
+++ src/test_preprocess.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "datatype.h"
+#include "preprocess.h"
+
+// unit test
+int main(void)
+{
+	std::string file("../input_data/root1_IOR/log.0");
+	Preprocess<str_hmap_list, str_hmap> pp(file);
+	pp.run();	
+	pp.data_print();
+}
